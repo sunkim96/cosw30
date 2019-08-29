bb81eadb5a0ac3d6d03d9da3500e37edf0137f9b
EC2 Default User <ec2-user@ip-172-31-40-75.ec2.internal>
2019-08-29 02:58:29 +0000

    Created lab 1 file and added ipsum

---
+++ homework/labwork/lab1.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+    <title>Labwork 1: Hello World</title>
+    <style></style>
+</head>
+    <body>
+        <h1>Sun Kim</h1>
+        <p></p>
+        <?php 
+            echo 'This is my PHP paragrpah';
+        ?>
+    </body>
+</html>
+
+
