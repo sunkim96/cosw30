b5338ca8da63170c9c9def1d6ba49cfece5ec2e1
EC2 Default User <ec2-user@ip-172-31-40-75.ec2.internal>
2019-09-24 06:16:29 +0000

    hw4 push with 2nd table

---
+++ homework/homework4/population.php
@@ -0,0 +1,29 @@
+<?php
+
+$city = array 
+    ( 
+        array("New York, New York", 8622698),
+        array("Los Angeles, California", 3999759),
+        array("Chicago, Illinois", 2716450),
+        array("Houston, Texas", 2312717),
+        array("Phoeniz, Arizona", 1626078),
+        array("Philadelphia, Pennsylvania", 1580862),
+        array("San Antonia, Texas", 1511946),
+        array("San Diego, California", 1419516),
+        array("Dallas, Texas", 1341075),
+        array("San Jose, California", 1035317),
+        array("Austin, Texas", 950715),
+        array("Jacksonville, Florida", 892062),
+        array("San Francisco, California", 884363),
+        array("Columbus", 879170),
+        array("Fort Worth", 874168)
+    
+    );
+
+echo ksort($city); 
+
+
+
+
+
+?>
