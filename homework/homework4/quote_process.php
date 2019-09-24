2ec21e66f4e9505e10300954c069bcc271d316ea
EC2 Default User <ec2-user@ip-172-31-40-75.ec2.internal>
2019-09-24 04:46:31 +0000

    new quote hw

--- homework/homework4/quote_process.php
+++ homework/homework4/quote_process.php
@@ -1,47 +1,18 @@
 <?php
 
 $text = $_POST['text'];
+$quote = array($text);
+$count = count($quote);
+// output the list originally with count
+echo "Your Original Quote: $quote $count";
 
-$textExplode = explode(' ', $text);
-
-$textlength = count($text);
-
-//output as original
-echo "<p>Your Original Quote:</p>";
-echo $textExplode;
-echo $textlength;
-
-
-//output the list alphabetized
-echo "<p>Your Quote Alphabetized:</p>";
-sort($text);
-
-for($i = 0; $i < $textlength; $i++) {
-    echo $text[$i];
+// output the list alphabetized
+sort($quote);
+for($i = 0; < $count; $i++) {
+    echo $quote[$i];
     echo "<br>";
 }
 
-
-//output the list reverse alphabetized 
-echo "<p>Your Quote In Reverse</p>";
-rsort($text);
-
-for($i = 0; $i < $textlength; $i++) {
-    echo $text[$i];
-    echo "<br>";
-}
-
-
-// add three new words, output text w/new array and count
-$great = array("Wow", "Amazing", "Great Job");
-
-echo "<p>Some Additional Words:</p>";
-echo $textExplode;
-for($i = 0; $i < $textlength; $i++) {
-    echo $text[$i];
-    echo $great;
-}
-echo $textlength; 
 
 
 ?>
