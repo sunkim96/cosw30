2cdeb0a63fc136fcffc7b0fb206dd52b3dead9ef
EC2 Default User <ec2-user@ip-172-31-40-75.ec2.internal>
2019-09-24 05:22:03 +0000

    hw 4 3rd commit

--- homework/homework4/quote_process.php
+++ homework/homework4/quote_process.php
@@ -4,16 +4,32 @@ $text = $_POST['text'];
  $textExplode = explode(' ', $text);
 $count = count($textExplode);
 // output the list originally with count
-echo "Your Original Quote: $textExplode $count";
+echo "Your Original Quote:";
+echo $textExplode;
+echo $count;
 
 // output the list alphabetized
 sort($textExplode);
+echo "<p>Your Quote Alphabetized: </p>";
 for($i = 0; $i < $count; $i++) {
-    echo "<p>Your Quote Alphabetized: </p>";
     echo $textExplode[$i];
     echo "<br>";
 }
 
+// output the list reverse alphabetized
+rsort($textExplode);
+echo "<p>Your Quote in Reverse: </p>";
+for($i = 0; $i < $count; $i++) {
+    echo $textExplode[$i];
+    echo "<br>";
+}
 
+// add 3 words to the end output new array list with count
+
+echo $textExplode;
+$new = array("You", "Got", "This");
+echo "New Words" . $new[0] . "," . $new[1] . "," . $new[2] . "." ;
+echo $count;
+echo count($new);
 
 ?>
