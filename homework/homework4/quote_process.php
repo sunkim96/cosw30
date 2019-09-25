361f551b036bcf63748596960bb98c1f4e9aa2e0
EC2 Default User <ec2-user@ip-172-31-40-75.ec2.internal>
2019-09-25 04:34:30 +0000

    last last commit

--- homework/homework4/quote_process.php
+++ homework/homework4/quote_process.php
@@ -26,9 +26,8 @@ for($i = 0; $i < $count; $i++) {
 
 // add 3 words to the end output new array list with count
 $new = array("You", "Got", "This");
-echo "<p>$text</p>";
-echo "<br>";
 echo "<p>New Words:</p>" . $new[0] . "" . $new[1] . "" . $new[2] . " " ;
+echo "<p>$text</p>";
 echo "<br>";
 echo $count;
 echo "<br>";
