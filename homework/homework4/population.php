b3165fc4722950aba1c4fbfaf9d1f06192e354bc
EC2 Default User <ec2-user@ip-172-31-40-75.ec2.internal>
2019-09-25 03:44:54 +0000

    table commit

--- homework/homework4/population.php
+++ homework/homework4/population.php
@@ -1,27 +1,27 @@
 <?php
 
-$city = array 
+$multiarray = array 
     ( 
-        array("New York, New York", 8622698),
-        array("Los Angeles, California", 3999759),
-        array("Chicago, Illinois", 2716450),
-        array("Houston, Texas", 2312717),
-        array("Phoenix, Arizona", 1626078),
-        array("Philadelphia, Pennsylvania", 1580862),
-        array("San Antonia, Texas", 1511946),
-        array("San Diego, California", 1419516),
-        array("Dallas, Texas", 1341075),
-        array("San Jose, California", 1035317),
-        array("Austin, Texas", 950715),
-        array("Jacksonville, Florida", 892062),
-        array("San Francisco, California", 884363),
-        array("Columbus", 879170),
-        array("Fort Worth", 874168)
+        array("name"=>"New York, New York", "value"=>8622698),
+        array("name"=>"Los Angeles, California", "value"=>3999759),
+        array("name"=>"Chicago, Illinois", "value"=>2716450),
+        array("name"=>"Houston, Texas", "value"=>2312717),
+        array("name"=>"Phoenix, Arizona", "value"=>1626078),
+        array("name"=>"Philadelphia, Pennsylvania", "value"=>1580862),
+        array("name"=>"San Antonia, Texas", "value"=>1511946),
+        array("name"=>"San Diego, California", "value"=>1419516),
+        array("name"=>"Dallas, Texas", "value"=>1341075),
+        array("name"=>"San Jose, California", "value"=>1035317),
+        array("name"=>"Austin, Texas", "value"=>950715),
+        array("name"=>"Jacksonville, Florida", "value"=>892062),
+        array("name"=>"San Francisco, California", "value"=>884363),
+        array("name"=>"Columbus", "value"=>879170),
+        array("name"=>"Fort Worth", "value"=>874168)
     
     );
 
 
-for ($row = 0; $row < 15; $row++) {
+/*for ($row = 0; $row < 15; $row++) {
   echo "<p><b>Row number $row</b></p>";
   echo "<ul>";
   for ($col = 0; $col < 2; $col++) {
@@ -29,7 +29,25 @@ for ($row = 0; $row < 15; $row++) {
   }
   echo "</ul>";
 }
+*/
 
+$count = 0;
+
+foreach ($multiarray as $arrays){
+    $count++;
+    echo "<table>" ;               
+
+    echo "<span>table $count</span>";
+    echo "<tr>";
+    foreach ($arrays as $names => $value){
+
+        echo "<th>$names</th>";
+        echo "<td>$value</td>";
+
+    }
+    echo "</tr>";
+    echo "</table>";
+}
 
 
 
