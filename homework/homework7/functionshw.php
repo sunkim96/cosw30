6192e9d80df22012d6761954a0abb2b33ee1fe60
EC2 Default User <ec2-user@ip-172-31-40-75.ec2.internal>
2019-10-12 03:45:02 +0000

    added second form input

--- homework/homework7/functionshw.php
+++ homework/homework7/functionshw.php
@@ -5,18 +5,36 @@
  }
  $value1 = $_POST['value1'];
  $value2 = $_POST['value2'];
+ 
+ function subtwo($c = 0, $d = 0)
+ {
+ return ($c - $d);
+ }
+ $value3 = $_POST['value3'];
+ $value4 = $_POST['value4'];
+ 
+ 
 ?>
 <html>
- <title>PHP - Add two numbers</title>
+ <title>PHP Formulas</title>
  <body>
  <form action="functionshw.php" method="post">
  <input type="text" name="value1" value="0" />
  <input type="text" name="value2" value="0" />
  <input type="submit" value="Calculate values"/>
- </form>
+
  <?php
  echo $value1 . " + " . $value2 . " = ";
  echo addtwo($value1+$value2); 
+ ?>
+
+ <input type="text" name="value3" value="0" />
+ <input type="text" name="value4" value="0" />
+ <input type="submit" value="Calculate values"/>
+ </form>
+ <?php
+ echo $value3 . " - " . $value4 . " = ";
+ echo subtwo($value3-$value4); 
  ?>
  </body>
 </html>
