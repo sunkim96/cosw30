f01dc01d32aafb1b83da96751f3f381cb427bb55
EC2 Default User <ec2-user@ip-172-31-40-75.ec2.internal>
2019-10-13 04:47:03 +0000

    changed function and set values

--- homework/homework7/functionshw.php
+++ homework/homework7/functionshw.php
@@ -1,6 +1,35 @@
 <!doctype html>
-
-<?php
+<html>
+ <head>
+        <meta charset="utf-8">
+        <title></title>
+    <style></style>
+    
+    
+    <body>
+   <form action="functionshw.php" method="post" class="form-inline">
+       <label></label>
+       <input type="text" name="firstvalue" value="<?php echo $firstvalue; ?>" placeholder="Insert Here" />
+  
+     
+      <select size="1" name="operations">
+         <option value="+">+</option>
+         <option value="-">-</option>
+         <option value="*">*</option>
+         <option value="/">/</option>
+      </select>
+     
+     <input type="text" name="secondvalue" value="<?php echo $secondvalue; ?>" placeholder="Insert Here" />
+  
+     <button type="submit" name="calculate">Calculate</button>
+     
+     <?php
+     $firstvalue = $_POST['firstvalue'];
+     $secondvalue = $_POST['secondvalue'];
+     
+     
+     
+     
       if ($_REQUEST['operations'] == '+')
       	{ 
           echo $firstvalue + $secondvalue; 
@@ -21,29 +50,10 @@
         echo $firstvalue / $secondvalue; 
  
       } ?>
-<html>
- <head>
-        <meta charset="utf-8">
-        <title></title>
-    <style></style>
-    
-    
-    <body>
-   <form action="functionshw.php" method="post">
-       <label></label>
-       <input type="text" name="firstvalue" value="<?php echo $firstvalue; ?>" placeholder="Insert Here" />
-  
      
-      <select size="1" name="operations">
-         <option value="+">+</option>
-         <option value="-">-</option>
-         <option value="*">*</option>
-         <option value="/">/</option>
-      </select>
      
-     <input type="text" name="secondvalue" value="<?php echo $secondvalue; ?>" placeholder="Insert Here" />
-  
-     <button type="submit" name="calculate">Calculate</button>
+     
+     
      </form>
      
      
